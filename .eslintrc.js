534183f6ccd6355c76e090659a5c4cab7d7317d4
Kevin Berthier <berthier.kevin.y@gmail.com>
2021-02-23 14:39:31 +0100

    replicate prettier config for non vue files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,68 +5,44 @@ module.exports = {
       modules: true,
     },
   },
+
   env: {
     browser: true,
     jquery: true,
     node: true,
   },
+
   extends: [
     'eslint:recommended',
     'standard',
     'plugin:import/errors',
-    //  Vue
     'plugin:vue/recommended',
     'plugin:prettier-vue/recommended',
-    'prettier/vue',
+    'prettier',
   ],
+
   settings: {
     'prettier-vue': {
       // Settings for how to process Vue SFC Blocks
       SFCBlocks: {
-        /**
-         * Use prettier to process `<template>` blocks or not
-         *
-         * If set to `false`, remember not to `extends: ['prettier/vue']`, as you need the rules from `eslint-plugin-vue` to lint `<template>` blocks
-         *
-         * @default true
-         */
         template: true,
-
-        /**
-         * Use prettier to process `<script>` blocks or not
-         *
-         * @default true
-         */
         script: true,
-
-        /**
-         * Use prettier to process `<style>` blocks or not
-         *
-         * @default true
-         */
         style: true,
-
-        // Settings for how to process custom blocks
         customBlocks: {
           // Treat the `<docs>` block as a `.markdown` file
           docs: { lang: 'markdown' },
-
           // Treat the `<config>` block as a `.json` file
           config: { lang: 'json' },
-
           // Treat the `<module>` block as a `.js` file
           module: { lang: 'js' },
-
           // Ignore `<comments>` block (omit it or set it to `false` to ignore the block)
           comments: false,
-
           // Other custom blocks that are not listed here will be ignored
         },
       },
 
       // Use prettierrc for prettier options or not (default: `true`)
       usePrettierrc: true,
-
       // Set the options for `prettier.getFileInfo`.
       // @see https://prettier.io/docs/en/api.html#prettiergetfileinfofilepath-options
       fileInfoOptions: {
